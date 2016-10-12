c6ffa63eeb6b14c1ccf38997c93ceaaedefb51d3
Morena <moresfever@hotmail.com>
2016-10-12 10:29:36 -0300

    sin ogmoloco y setteada la vida del boss

--- source/sprites/Boss.hx
+++ source/sprites/Boss.hx
@@ -2,18 +2,36 @@ package sprites;
 
 import flixel.FlxSprite;
 import flixel.system.FlxAssets.FlxGraphicAsset;
+import flixel.FlxG;
 
 /**
  * ...
  * @author ...
  */
 class Boss extends FlxSprite
-{
+	{ private var vidaboss:Int = 50;
 
 	public function new(?X:Float=0, ?Y:Float=0, ?SimpleGraphic:FlxGraphicAsset) 
-	{
+	{ 
 		super(X, Y, SimpleGraphic);
-		
+		if (FlxG.overlap(this, Personaje.bala))
+		   {
+			   Personaje.bala.destroy();
+			   vidaboss -= 1;
+			   
+		   }
+			if (vidaboss < 0)
+			this.destroy();
 	}
+
+	public function setVida (v:Int): Void
+		{
+			vidaboss = v;
+		}
+		
+		public function getVida ():Int
+		{
+			return vidaboss;
+		}
 	
 }
